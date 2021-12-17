a873f1aa2e5a2467728151bc7e1b2fbee644ccad
Nikita <nikita.n.borisov@gmail.com>
2021-12-17 13:34:11 +0300

    Initial Commit

---
+++ DropPlay/DropPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropPlayApp.swift
+//  DropPlay
+//
+//  Created by Borisov Nikita on 17.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DropPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
